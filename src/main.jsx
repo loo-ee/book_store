497ddee836fcd3a1a48ca5ad970ea9ca2dbe2e24
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2022-11-20 18:58:28 +0800

    Remove media that are subject to copyright

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/index.css';
 
 const books = [
   {
     id: 1,
     title: `Python Crash Course, 2nd Edition: A Hands-On, Project-Based Introduction to Programming 2nd Edition`,
     price: 14.8,
-    source: 'python-crash-course.jpg',
+    source: 'tool-box.png',
     availability: 'Limited',
     author: 'Eric Matthes',
     ratings: '7, 630',
@@ -18,7 +18,7 @@ const books = [
     id: 2,
     title: 'HTML and CSS: Design and Build Websites 1st Edition',
     price: 13.9,
-    source: 'html-css.jpg',
+    source: 'tool-box.png',
     availability: 'In Stock',
     author: 'Jon Duckett',
     ratings: '4, 162',
@@ -29,7 +29,7 @@ const books = [
     // eslint-disable-next-line
     title: `JavaScript: The Definitive Guide: Master the World's Most-Used Programming Language 7th Edition`,
     price: 20.12,
-    source: 'javascript.jpg',
+    source: 'tool-box.png',
     availability: 'In Stock',
     author: 'David Flanagan',
     ratings: '1, 094',
@@ -45,10 +45,10 @@ const books = [
     ratings: '???',
     genre: '???',
   },
-]
+];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App books={books} />
   </React.StrictMode>
-)
+);
